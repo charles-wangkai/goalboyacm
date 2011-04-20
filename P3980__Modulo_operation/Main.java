a4d9fa27ef286e3f63157e894cb9093cf739a2b3
charles.wangkai <charles.wangkai@fcd8bc74-a92c-3fdb-14d6-58a2fc0a772d>
2011-04-20 13:34:07 +0000

    P3980

---
+++ P3980__Modulo_operation/Main.java
@@ -0,0 +1,14 @@
+package P3980__Modulo_operation;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNextInt()) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			System.out.println(a % b);
+		}
+	}
+}
